25ff9eded4979c9d7531b633d976ea9fba9503b4
nicey <laisi1127@163.com>
2022-10-13 11:45:01 +0800

    初始化仓库

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Nicey></Nicey>
+</template>
+
+<script>
+import Nicey from './components/Nicey.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Nicey
+  }
+}
+</script>
