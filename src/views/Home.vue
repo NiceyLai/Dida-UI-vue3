a6815ade3b9e9e790d33b35f5c3f38fc2f676614
nicey <laisi1127@163.com>
2022-10-13 15:46:01 +0800

    修改topnav 样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
+       <div class="logo">LOGO</div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
     </div>
     <div class="banner">
       <h1>点滴UI</h1>
@@ -16,6 +19,22 @@
 </template>
 
 <style lang="scss" scoped>
-  .topnar{border: 1px solid red;}
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
   
 </style>
