5483b0b218918232b794be304f69ac5daf525219
nicey <laisi1127@163.com>
2022-10-15 09:32:42 +0800

    Topnav 组件封装

--- src/components/Nicey2.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        我的第er个组件
-    </div>
-</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <div class="topnav">
-       <div class="logo">LOGO</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-    </div>
+<Topnav />
     <div class="banner">
       <h1>点滴UI</h1>
       <h2>一个厉害的UI框架</h2>
@@ -18,23 +12,12 @@
   </div>
 </template>
 
-<style lang="scss" scoped>
-.topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
-    }
-  }
+<script lang="ts">
+import Topnav from '../components/Topnav.vue'
+export default {
+    components: { Topnav }
 }
-  
+</script>
+<style lang="scss" scoped>
+
 </style>
