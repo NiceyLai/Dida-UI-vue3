0dde7707b486a7fb5c81731d2bd909fc30a9c27e
nicey <laisi1127@163.com>
2022-10-16 16:24:21 +0800

    aside css 1

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -30,3 +30,23 @@ export default {
   components: {Topnav} 
 }
 </script>
+
+ript>
+<style lang="scss" scoped>
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+</style>
