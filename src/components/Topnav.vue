dd0ad25d9c23015f182727ca4a3e804357b833a1
nicey <laisi1127@163.com>
2022-10-16 22:42:19 +0800

    添加 toggleAside

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -48,7 +48,14 @@ export default {
     }
   }
   .toggleAside{
-    
+     display: inline-block;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
   }
   @media (max-width:500px) {
     >.menu{
