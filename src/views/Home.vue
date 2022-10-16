48cd77e439606835918a935c75b7b6556ea57140
nicey <laisi1127@163.com>
2022-10-16 16:23:08 +0800

    初始化 aside 的 html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>点滴UI</h1>
       <h2>一个厉害的UI框架</h2>
       <p class="actions">
-        <a href="">GitHub</a>
-        <a href="">开始</a>
+        <a href="https://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
