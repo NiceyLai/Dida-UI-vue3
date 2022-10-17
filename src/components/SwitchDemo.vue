240e74558716b2ccc5ae151203f4f8df29ba2c2a
nicey <laisi1127@163.com>
2022-10-17 14:36:56 +0800

    Switch组件添加HTML和CSS

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
-        switch文档
+        <Switch />
     </div>
 </template>
+
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+export default {
+    components: { Switch }
+}
+</script>
