8b6b4cccbce813e132529b4bed82382f12023758
nicey <laisi1127@163.com>
2022-10-18 10:45:16 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
-    DialogDemo
+    Dialog 示例
     </div>
+    <h1>示例1</h1>
+    <Dialog></Dialog>
 </template>
+
+<script lang="ts">
+import  Dialog from '../lib/Dialog.vue'
+export default {
+    components: { Dialog },
+    setup() {
+       
+    }
+}
+</script>
