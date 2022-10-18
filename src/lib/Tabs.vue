26284b715c86611ee6b3cc9bbc6e8e1fec0fd405
nicey <laisi1127@163.com>
2022-10-18 21:08:12 +0800

    渲染 Tabs 内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,8 +1,7 @@
 <template>
 <div>
-  Tabs 组件
-  <component :is="defaults[0]" />
-  <component :is="defaults[1]" />
+  <div v-for="(t,index) in titles" :key="index">{{t}}</div>
+  <component v-for='(c,index) in defaults' :is="c" :key="index"/>
 </div>
 </template>
 
@@ -16,8 +15,11 @@ export default {
                 throw new Error ('Tabs 子标签必须是Tab')
             }
         })
+        const titles= defaults.map((tag) => {
+            return tag.props.title;
+        })
         return {
-            defaults
+            defaults,titles
         }
         
     }
