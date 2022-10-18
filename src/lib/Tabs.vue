a8dd723be110f1c616894e0f169a0d5f534ac092
nicey <laisi1127@163.com>
2022-10-18 21:45:51 +0800

    使用 CSS 切换内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,17 @@
 <template>
-<div class="gulu-tabs">
-  <div class="gulu-tabs-nav">
-    <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+<div class="dida-tabs">
+  <div class="dida-tabs-nav">
+    <div class="dida-tabs-nav-item" 
+    @click="select(t)"
+    :class="{selected:t===selected}"
+    v-for="(t,index) in titles" :key="index">{{t}}</div>
   </div>
-  <div class="gulu-tabs-content">
-    <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+  <div class="dida-tabs-content">
+    <component class="dida-tabs-content-item"
+     :class="{selected: c.props.title === selected}"
+       v-for="(c,index) in defaults" 
+    :is="c" :key="index" 
+     />
   </div>
 </div>
 </template>
@@ -12,6 +19,11 @@
 <script lang="ts">
 import Tab from './Tab.vue'
 export default {
+    props: {
+        selected: {
+            type:String
+        }
+    },
     setup(props, context) {
         const defaults = context.slots.default();
         defaults.forEach((tag) => {
@@ -22,8 +34,11 @@ export default {
         const titles= defaults.map((tag) => {
             return tag.props.title;
         })
+          const select = (title: string) => {
+      context.emit('update:selected', title)
+    }
         return {
-            defaults,titles
+            defaults,titles,select
         }
         
     }
@@ -35,7 +50,7 @@ export default {
 $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
-.gulu-tabs {
+.dida-tabs {
   &-nav {
     display: flex;
     color: $color;
@@ -54,6 +69,12 @@ $border-color: #d9d9d9;
   }
   &-content {
     padding: 8px 0;
+    &-item{
+        display: none;
+        &.selected {
+            display:block;
+        }
+    }
   }
 }
 </style>
