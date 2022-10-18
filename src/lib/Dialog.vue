2ad3b4c070bce70af616b2a527fae4d40642201b
nicey <laisi1127@163.com>
2022-10-18 16:11:58 +0800

    openDialog 一句话打开 Dialog

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -58,7 +58,7 @@ export default {
       }
     };
     const cancel = () => {
-      context.emit("cancel");
+      props.cancel?.()
       close();
     };
     return { close, onClickOverlay, ok, cancel };
@@ -69,12 +69,14 @@ export default {
 <style lang="scss">
 $radius: 4px;
 $border-color: #d9d9d9;
+
 .dida-dialog {
   background: white;
   border-radius: $radius;
   box-shadow: 0 0 3px fade_out(black, 0.5);
   min-width: 15em;
   max-width: 90%;
+
   &-overlay {
     position: fixed;
     top: 0;
@@ -84,6 +86,7 @@ $border-color: #d9d9d9;
     background: fade_out(black, 0.5);
     z-index: 10;
   }
+
   &-wrapper {
     position: fixed;
     left: 50%;
@@ -91,7 +94,8 @@ $border-color: #d9d9d9;
     transform: translate(-50%, -50%);
     z-index: 11;
   }
-  > header {
+
+  >header {
     padding: 12px 16px;
     border-bottom: 1px solid $border-color;
     display: flex;
@@ -99,20 +103,24 @@ $border-color: #d9d9d9;
     justify-content: space-between;
     font-size: 20px;
   }
-  > main {
+
+  >main {
     padding: 12px 16px;
   }
-  > footer {
+
+  >footer {
     border-top: 1px solid $border-color;
     padding: 12px 16px;
     text-align: right;
   }
+
   &-close {
     position: relative;
     display: inline-block;
     width: 16px;
     height: 16px;
     cursor: pointer;
+
     &::before,
     &::after {
       content: "";
@@ -123,9 +131,11 @@ $border-color: #d9d9d9;
       top: 50%;
       left: 50%;
     }
+
     &::before {
       transform: translate(-50%, -50%) rotate(-45deg);
     }
+
     &::after {
       transform: translate(-50%, -50%) rotate(45deg);
     }
