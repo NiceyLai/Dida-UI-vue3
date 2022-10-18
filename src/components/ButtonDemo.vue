9a29e3fd1e62f8c3a3e0d7ac34cd46892ab6aa02
nicey <laisi1127@163.com>
2022-10-18 09:55:29 +0800

    基本完善 Button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -50,6 +50,11 @@
     <Button theme="link" disabled>禁用链接按钮</Button>
     <Button theme="text" disabled>禁用按钮</Button>
   </div>
+   <h1>示例5</h1>
+  <div>
+    <Button loading>加载中</Button>
+    <Button>加载完毕</Button>
+  </div>
 </template>
 
 <script lang="ts">
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
   <button class="dida-button" :class="classes" :disabled="disabled">
+   <span v-if="loading" class="dida-loadingIndicator"></span>
     <slot />
   </button>
 </template>
@@ -24,6 +25,10 @@ export default {
       type: Boolean,
       default:false,
     },
+     loading: {
+      type: Boolean,
+      default: false
+    }
   },
   setup(props) {
     const { theme, size, level } = props;
@@ -166,5 +171,20 @@ $grey: grey;
       color: $grey;
     }
   }
+  > .dida-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px; 
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: dida-spin 1s infinite linear;
+  }
+}
+@keyframes dida-spin {
+  0%{transform: rotate(0deg)} 
+  100%{transform: rotate(360deg)} 
 }
 </style>
