d47ac827b447a61dd1f7daf82a86919d72d6bc87
nicey <laisi1127@163.com>
2022-10-19 16:08:54 +0800

    高亮

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -51,9 +51,7 @@ export default {
 </script>
 
 <style lang="scss" >
-.router-link-active {
-  text-decoration: underline;
-}
+
 .layout {
   display: flex;
   flex-direction: column;
@@ -91,7 +89,7 @@ export default {
 aside {
   background: lightblue;
   width: 150px;
-  padding: 16px;
+  padding: 16px 0;
   position: fixed;
   top: 0;
   left: 0;
@@ -100,11 +98,19 @@ aside {
 
   > h2 {
     margin-bottom: 4px;
+    padding: 0 16px;
   }
 
   > ol {
     > li {
-      padding: 4px 0;
+      >a {
+        display: block;
+        padding: 4px 16px;
+        text-decoration: none;
+      }
+      .router-link-active {
+        background: white;
+      }
     }
   }
 
