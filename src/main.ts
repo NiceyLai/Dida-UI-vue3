32636dbef560864b449a9e89836740a75768145c
nicey <laisi1127@163.com>
2022-10-19 17:15:52 +0800

    引入GitHub样式

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import "./lib/dida.scss";
 import "./index.scss";
+import "github-markdown-css";
 
 import { router } from "./router";
 import { createApp } from "vue";
 import App from "./App.vue";
+
 
 const app = createApp(App);
 app.use(router);
