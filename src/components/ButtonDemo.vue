bae8d9f99cf4d268a9d92a410d68dd11594878d1
nicey <laisi1127@163.com>
2022-10-21 22:45:36 +0800

    暂时提交

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Button 示例</h1>
+    <h1>Button 按钮</h1>
     <Demo :component="Button1Demo" />
     <Demo :component="Button2Demo" />
     <Demo :component="Button3Demo" />
